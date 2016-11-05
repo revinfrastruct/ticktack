7443296e53625a03bfa1e7686af7c4c64ecedf7d
alfred <alfred@nurd.nu>
2016-11-05 17:31:48 +0000

    moved ticks stuff to its own module

--- test/ticks.js
+++ test/ticks.js
@@ -1,21 +1,21 @@
 const expect = require('chai').expect;
 const testdouble = require('testdouble');
-const ticktack = require('..');
+const ticks = require('../src/ticks');
 const url = require('url');
 
-describe('Ticktack', () => {
+describe('ticks', () => {
 
 	describe('init()', () => {
 		it('should load configuration from config.json', () => {
-			ticktack.config.load = testdouble.function();
-			ticktack.init();
-			testdouble.verify(ticktack.config.load('config.json'));
+			ticks.config.load = testdouble.function();
+			ticks.init();
+			testdouble.verify(ticks.config.load('config.json'));
 		});
 	});
 
 	describe('get_bucket()', () => {
 		it('returns a non-empty string', () => {
-			return ticktack.get_bucket()
+			return ticks.get_bucket()
 			.then(bucket => {
 				expect(bucket).to.be.a('string');
 				expect(bucket).to.not.equal('');
@@ -25,7 +25,7 @@ describe('Ticktack', () => {
 
 	describe('get_region()', () => {
 		it('returns a non-empty string', () => {
-			return ticktack.get_region()
+			return ticks.get_region()
 			.then(region => {
 				expect(region).to.be.a('string');
 				expect(region).to.not.equal('');
@@ -35,7 +35,7 @@ describe('Ticktack', () => {
 
 	describe('get_tickername()', () => {
 		it('returns a non-empty string', () => {
-			return ticktack.get_tickername()
+			return ticks.get_tickername()
 			.then(ticker => {
 				expect(ticker).to.be.a('string');
 				expect(ticker).to.not.equal('');
@@ -45,7 +45,7 @@ describe('Ticktack', () => {
 
 	describe('static_website_url()', () => {
 		it('returns a non-empty string', () => {
-			return ticktack.static_website_url()
+			return ticks.static_website_url()
 			.then(ticks_url => {
 				expect(ticks_url).to.be.a('string');
 				const urlparts = url.parse(ticks_url);
@@ -69,20 +69,20 @@ describe('Ticktack', () => {
 				}
 			};
 			before(() => {
-				ticktack.http.get = testdouble.function();
-				return ticktack.static_website_url()
+				ticks.http.get = testdouble.function();
+				return ticks.static_website_url()
 				.then(ticks_url => {
-					testdouble.when(ticktack.http.get(ticks_url)).thenResolve(test_data);
+					testdouble.when(ticks.http.get(ticks_url)).thenResolve(test_data);
 				});
 			});
 			it('should load memory with ticks', () => {
-				return ticktack.load_ticks()
+				return ticks.load_ticks()
 				.then(data => {
-					expect(ticktack.ticks).to.deep.equal(test_data);
+					expect(ticks.ticks).to.deep.equal(test_data);
 					
 					// They should not be the same object, just idential objects:
 					test_data['10'].content = '<p>Something else</p>';
-					expect(ticktack.ticks).to.not.deep.equal(test_data);
+					expect(ticks.ticks).to.not.deep.equal(test_data);
 				});
 			});
 		});
@@ -95,16 +95,16 @@ describe('Ticktack', () => {
 				}
 			};
 			before(() => {
-				ticktack.http.get = testdouble.function();
-				return ticktack.static_website_url()
+				ticks.http.get = testdouble.function();
+				return ticks.static_website_url()
 				.then(ticks_url => {
-					testdouble.when(ticktack.http.get(ticks_url)).thenResolve(test_data);
+					testdouble.when(ticks.http.get(ticks_url)).thenResolve(test_data);
 				});
 			});
 			it('should not keep old (key "4") data after loading batch with deleted ticks', () => {
-				return ticktack.load_ticks()
+				return ticks.load_ticks()
 				.then(data => {
-					expect(ticktack.ticks).to.deep.equal(test_data);
+					expect(ticks.ticks).to.deep.equal(test_data);
 				});
 			});
 		});
